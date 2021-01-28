b76a0702a3d0c8784a254d2f4f8fa8f5089d3dd6
raquelalcazar <71818474+raquelalcazar@users.noreply.github.com>
2021-01-28 09:19:03 +0100

    Create pe_inicio.php

---
+++ pe_inicio.php
@@ -0,0 +1,35 @@
+<?php
+	session_start();
+	include_once("funciones.php");
+
+	if (isset($_SESSION["usuario"])) {
+
+		$id = $_SESSION["usuario"];
+		$datos = obtenerNombre($id);
+
+	} else {
+
+		// Si el usuario NO ha iniciado sesión se redirige a "pe_login.php"
+		header("location: pe_login.php"); 
+		
+	}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Web Compras</title>
+	<meta charset="utf-8" />
+	<meta name="author" value="Raquel Alcázar" />
+</head>
+<body>
+	<h1>Bienvenido <?php echo $datos["username"]; ?></h1>
+	<li><a href="pe_login.php">Cerrar sesión</a></li>
+	<li><a href="pe_altaped.php">Alta de pedidos</a></li>
+	<li><a href="pe_consped.php">Consultar pedidos</a></li>
+	<li><a href="pe_consprodstock.php">Consultar stock de producto</a></li>
+	<li><a href="pe_constock.php">Consultar stock de línea</a></li>
+	<li><a href="pe_topprod.php">Consultar ventas</a></li>
+	<li><a href="pe_conspago.php">Consultar compras</a></li>
+</body>
+</html>
