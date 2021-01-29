8644abf6407c9451feaa81e65903013cd0193ab8
LucasFadavi91 <71818752+LucasFadavi91@users.noreply.github.com>
2021-01-29 12:21:53 +0100

    Add files via upload

--- pe_consprodstock.php
+++ pe_consprodstock.php
@@ -7,12 +7,11 @@
     </HEAD>
     <BODY>
 <?php
-        include_once 'funcionesDani.php';
-        $pdo=connectDBpedidos();
-  
+        include_once 'funciones.php';
+    
     try {
 
-       $productos = obtenerStock($pdo);  
+       $productos = obtenerStock();  
     
        if (!isset($_POST) || empty($_POST)) { 
            
@@ -37,15 +36,46 @@
         }else{
     
             $seleccionado=$_POST['productos'];
-			$seleccionado=substr($seleccionado, 0, 4);//Recorto el string por si hubiese problemas por los espacios
-			echo $seleccionado."<br>";
+			//$seleccionado=substr($seleccionado, 0, 4);//Recorto el string por si hubiese problemas por los espacios
+			//echo $seleccionado."<br>";
 			
 			/*No sé si usar una variable dentro de una secuencia es posible y por eso da error*/
 	       /*El error consiste en que siempre muestra que el stock es 0, supongo que no detecta bien la variable*/
-			$sql="SELECT quantityInStock FROM products WHERE productName LIKE '".$seleccionado."%'";
-			$uwu=$pdo->exec($sql);
-			echo $uwu;
-			
+           $uwu = $conexion->prepare("SELECT quantityInStock, productname FROM products WHERE productName LIKE '".$seleccionado."%'");
+            //$uwu=$conexion->exec($sql);
+            $uwu->execute();
+			//echo $uwu;
+            foreach($uwu->fetchAll() as $row) {
+                echo "El producto  " .$row["productname"]. " dispone de <strong>" .$row["quantityInStock"]. "</strong> unidades.<br>";
+            
+            
+            }
+            /*Modificaciones:
+                He comentado el substr porque al reducir el nombre del producto seleccionaba varios
+                productos que tenian esa cadena de string en comun.
+                he añadido un prepare al select.
+                muestro los resultados con un bucle "que igual se puede omitir ya que solo deberia
+                devolver un resultado o eso entiendo"
+                he modificado el parametro que se le pasa a las funciones $pdo 
+                por una conexion directa mediante el archivo conexion.php que 
+                se incluye en las funciones.
+                ¡Modifico el include_once de la linea 10, cuidado a la hora de realizar pruebas!.
+                
+                function obtenerStock() {
+
+    global $conexion;
+
+    $stock = array();
+    $sql = "SELECT productName, quantityInStock FROM products";
+
+    foreach ($conexion->query($sql) as $row) {
+        $productos[]=$row['productName'];   
+
+    }
+    return $productos;
+} 
+                
+                */
         }    
     }catch(PDOException $e)
         {  
